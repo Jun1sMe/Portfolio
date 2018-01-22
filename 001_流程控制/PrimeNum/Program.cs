dc3be577e9cdf18a8038efa56f973158f811bd12
Ostynn <sgnileef6@gmail.com>
2018-01-22 21:33:03 +0800

    init

---
+++ 001_流程控制/PrimeNum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimeNum
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            while(true)
+            {
+				Console.Write("Please input an integer: ");
+				String input = Console.ReadLine();
+				int num = Int32.Parse(input);
+				if (num < 2)
+					Console.WriteLine("false");
+				else
+				{
+					int i;
+					for (i = 2; i < num; i++)
+						if (num % i == 0)
+							break;
+					if (i == num)
+						Console.WriteLine("true");
+					else
+						Console.WriteLine("false");
+				}                
+            }
+        }
+    }
+}
